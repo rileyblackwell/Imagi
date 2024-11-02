91de19aec6ce172286937eef5acc103649e83389
Riley Blackwell <rileyblackwell5@gmail.com>
2024-11-01 20:22:59 -0400

    renamed index.html

--- apps/Home/views.py
+++ apps/Home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def landing_page(request):
-    return render(request, 'home/index.html')
+    return render(request, 'home/home_landing_page.html')
 
