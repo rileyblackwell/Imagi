d5a8e441adc431ddd97c60d01add6fe985f5afae
Riley Blackwell <rileyblackwell5@gmail.com>
2024-12-10 16:51:28 -0500

    updated builder admin

--- apps/Builder/admin.py
+++ apps/Builder/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Conversation, Message
+from .models import Conversation, Message, Project, Page
 
 # Register your models here.
 admin.site.register(Conversation)
 admin.site.register(Message)
+admin.site.register(Project)
+admin.site.register(Page)
