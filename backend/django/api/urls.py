241b27c72822378e0debd226adbbb3ef1b1267cf
Riley Blackwell <rileyblackwell5@gmail.com>
2025-02-01 19:48:19 -0500

    deleted unneccesary django api app

--- backend/django/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('me/', views.UserProfileView.as_view(), name='user-profile'),
-    path('projects/', views.ProjectListCreateView.as_view(), name='project-list'),
-    path('projects/<int:pk>/', views.ProjectDetailView.as_view(), name='project-detail'),
-    path('contact/', views.ContactView.as_view(), name='contact'),
-] 
