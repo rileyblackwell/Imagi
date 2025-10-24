2a0eac565bbc80fd29641acd53db9a922769c1f2
Riley Blackwell <rileyblackwell5@gmail.com>
2025-10-23 20:17:19 -0400

    removed csrf console logging

--- frontend/vuejs/src/apps/auth/services/api.ts
+++ frontend/vuejs/src/apps/auth/services/api.ts
@@ -31,7 +31,6 @@ let logoutPromise: Promise<any> | null = null
 export const AuthAPI = {
   async getCSRFToken() {
     try {
-      console.log('Fetching CSRF token...')
       const response = await axios.get(`http://backend.railway.internal:8000/${API_PATH}/csrf/`, {
         timeout: 30000,
         headers: {
